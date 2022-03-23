d15dc0845cc560c792707143fe1f59a472fd8b69
yair1641 <100574534+yair1641@users.noreply.github.com>
2022-03-23 23:58:13 +0200

    Add files via upload

---
+++ DS_Intro_HW_3.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 16:12:26 2022
+
+@author: Yair Langerman 204156434
+"""
+
+
+def check_input(n, file):
+    if type(n) != int:
+        return False
+    with open(file.txt)
+        if NumOfLine != in file:
+        return False
+    return True
+
+
+from os.path import exists as file_exists
+
+file_exists('file')
+
+
+def read_line(n, file) 
+    if type(n) != int and type(file) != str:
+
+
+        
+
+
+
